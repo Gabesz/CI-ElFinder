22a763e242fbd8e34d8cc36f83d16fc06d257ce9
Gabesz <tgabesz@gmail.com>
2017-06-28 13:19:36 +0200

    Create Admin.php

---
+++ controllers/Admin.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+  	
+	public function __construct()
+	{
+		parent::__construct();
+	}
+  
+  public function elfinder_init()
+	{
+		$this->load->helper('path');
+		$hash = $volumeId . rtrim(strtr(base64_encode($path), '+/=', '-_.'), '.');
+		  $opts = array(
+		    'debug' => true, 
+		    'roots' => array(
+		      array( 
+		        'driver' => 'LocalFileSystem', 
+		        'path'   => set_realpath('uploads'), 
+        		'URL'    => site_url('uploads') . '/', 
+		      ) 
+		    )
+		  );
+		$this->load->library('elfinder_lib', $opts);
+	
+	}
+  
+  
+}
